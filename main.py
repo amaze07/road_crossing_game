c719dce0281d90d8d45a359d310bf1ace2087124
amaze07 <89767281+amaze07@users.noreply.github.com>
2023-02-25 23:48:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen, Turtle
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+scoreboard = Scoreboard()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    # player reaching other end
+    if player.ycor() > 270:
+        scoreboard.increase_level()
+        player.clear()
+        player.goto(0, -280)
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
